1861c3d8b323457282a5518784e4d3b52ace47f2
tazalov <tazalov9@gmail.com>
2024-05-18 12:46:41 +0300

    chore: prettier write

--- src/shared/api/queries/users.generated.tsx
+++ src/shared/api/queries/users.generated.tsx
@@ -1,8 +1,9 @@
-import * as Types from '../../types/apollo'
-
 import { gql } from '@apollo/client'
 import * as Apollo from '@apollo/client'
+
+import * as Types from '../../types/apollo'
 const defaultOptions = {} as const
+
 export type GetUserQueryVariables = Types.Exact<{
   id: Types.Scalars['Int']['input']
 }>
@@ -11,9 +12,9 @@ export type GetUserQuery = {
   __typename?: 'Query'
   getUser: {
     __typename?: 'ProfileModel'
-    firstname?: string | null
-    lastname?: string | null
-    aboutMe?: string | null
+    aboutMe?: null | string
+    firstname?: null | string
+    lastname?: null | string
   }
 }
 
@@ -44,22 +45,25 @@ export const GetUserDocument = gql`
  * });
  */
 export function useGetUserQuery(
-  baseOptions: Apollo.QueryHookOptions<GetUserQuery, GetUserQueryVariables> &
-    ({ variables: GetUserQueryVariables; skip?: boolean } | { skip: boolean })
+  baseOptions: ({ skip: boolean } | { skip?: boolean; variables: GetUserQueryVariables }) &
+    Apollo.QueryHookOptions<GetUserQuery, GetUserQueryVariables>
 ) {
   const options = { ...defaultOptions, ...baseOptions }
+
   return Apollo.useQuery<GetUserQuery, GetUserQueryVariables>(GetUserDocument, options)
 }
 export function useGetUserLazyQuery(
   baseOptions?: Apollo.LazyQueryHookOptions<GetUserQuery, GetUserQueryVariables>
 ) {
   const options = { ...defaultOptions, ...baseOptions }
+
   return Apollo.useLazyQuery<GetUserQuery, GetUserQueryVariables>(GetUserDocument, options)
 }
 export function useGetUserSuspenseQuery(
   baseOptions?: Apollo.SuspenseQueryHookOptions<GetUserQuery, GetUserQueryVariables>
 ) {
   const options = { ...defaultOptions, ...baseOptions }
+
   return Apollo.useSuspenseQuery<GetUserQuery, GetUserQueryVariables>(GetUserDocument, options)
 }
 export type GetUserQueryHookResult = ReturnType<typeof useGetUserQuery>
