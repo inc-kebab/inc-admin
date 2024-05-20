3ee1e0a5c8b4cf40467259843b969680b67197c5
tazalov <tazalov9@gmail.com>
2024-05-20 17:17:51 +0300

    IN-289_1 feat: add layout, draft pages

--- src/shared/api/queries/users.generated.tsx
+++ src/shared/api/queries/users.generated.tsx
@@ -1,32 +1,25 @@
-import { gql } from '@apollo/client'
-import * as Apollo from '@apollo/client'
+import * as Types from '../../types/apollo';
 
-import * as Types from '../../types/apollo'
-const defaultOptions = {} as const
-
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+const defaultOptions = {} as const;
 export type GetUserQueryVariables = Types.Exact<{
-  id: Types.Scalars['Int']['input']
-}>
+  id: Types.Scalars['Int']['input'];
+}>;
 
-export type GetUserQuery = {
-  __typename?: 'Query'
-  getUser: {
-    __typename?: 'ProfileModel'
-    aboutMe?: null | string
-    firstname?: null | string
-    lastname?: null | string
-  }
-}
+
+export type GetUserQuery = { __typename?: 'Query', getUser: { __typename?: 'ProfileModel', firstname?: string | null, lastname?: string | null, aboutMe?: string | null } };
+
 
 export const GetUserDocument = gql`
-  query GetUser($id: Int!) {
-    getUser(id: $id) {
-      firstname
-      lastname
-      aboutMe
-    }
+    query GetUser($id: Int!) {
+  getUser(id: $id) {
+    firstname
+    lastname
+    aboutMe
   }
-`
+}
+    `;
 
 /**
  * __useGetUserQuery__
@@ -44,29 +37,19 @@ export const GetUserDocument = gql`
  *   },
  * });
  */
-export function useGetUserQuery(
-  baseOptions: ({ skip: boolean } | { skip?: boolean; variables: GetUserQueryVariables }) &
-    Apollo.QueryHookOptions<GetUserQuery, GetUserQueryVariables>
-) {
-  const options = { ...defaultOptions, ...baseOptions }
-
-  return Apollo.useQuery<GetUserQuery, GetUserQueryVariables>(GetUserDocument, options)
-}
-export function useGetUserLazyQuery(
-  baseOptions?: Apollo.LazyQueryHookOptions<GetUserQuery, GetUserQueryVariables>
-) {
-  const options = { ...defaultOptions, ...baseOptions }
-
-  return Apollo.useLazyQuery<GetUserQuery, GetUserQueryVariables>(GetUserDocument, options)
-}
-export function useGetUserSuspenseQuery(
-  baseOptions?: Apollo.SuspenseQueryHookOptions<GetUserQuery, GetUserQueryVariables>
-) {
-  const options = { ...defaultOptions, ...baseOptions }
-
-  return Apollo.useSuspenseQuery<GetUserQuery, GetUserQueryVariables>(GetUserDocument, options)
-}
-export type GetUserQueryHookResult = ReturnType<typeof useGetUserQuery>
-export type GetUserLazyQueryHookResult = ReturnType<typeof useGetUserLazyQuery>
-export type GetUserSuspenseQueryHookResult = ReturnType<typeof useGetUserSuspenseQuery>
-export type GetUserQueryResult = Apollo.QueryResult<GetUserQuery, GetUserQueryVariables>
+export function useGetUserQuery(baseOptions: Apollo.QueryHookOptions<GetUserQuery, GetUserQueryVariables> & ({ variables: GetUserQueryVariables; skip?: boolean; } | { skip: boolean; }) ) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<GetUserQuery, GetUserQueryVariables>(GetUserDocument, options);
+      }
+export function useGetUserLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetUserQuery, GetUserQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<GetUserQuery, GetUserQueryVariables>(GetUserDocument, options);
+        }
+export function useGetUserSuspenseQuery(baseOptions?: Apollo.SuspenseQueryHookOptions<GetUserQuery, GetUserQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useSuspenseQuery<GetUserQuery, GetUserQueryVariables>(GetUserDocument, options);
+        }
+export type GetUserQueryHookResult = ReturnType<typeof useGetUserQuery>;
+export type GetUserLazyQueryHookResult = ReturnType<typeof useGetUserLazyQuery>;
+export type GetUserSuspenseQueryHookResult = ReturnType<typeof useGetUserSuspenseQuery>;
+export type GetUserQueryResult = Apollo.QueryResult<GetUserQuery, GetUserQueryVariables>;
