446e32f851b1b2c94159eca0dc0063788249fa10
Archi999999 <archi999999@gmail.com>
2024-06-03 13:50:39 +0300

    fix: add pagination user list in url params

--- src/feature/search-users/model/hooks/useSearchUsers.ts
+++ src/feature/search-users/model/hooks/useSearchUsers.ts
@@ -1,15 +1,29 @@
 import { useState } from 'react'
 
 import { useDebounce } from '@/shared/hooks'
+import { useRouter } from 'next/router'
 
 export const useSearchUsers = (onChangePage: (page: number) => void) => {
-  const [searchTerm, setSearchTerm] = useState('')
+  const router = useRouter()
+  const { term } = router.query
+  const [searchTerm, setSearchTerm] = useState((term as string) || '')
 
   const debouncedSearchTerm = useDebounce({ delay: 500, value: searchTerm })
 
   const handleSearch = (term: string) => {
     setSearchTerm(term)
     onChangePage(1)
+
+    const updatedQuery = { ...router.query }
+
+    if (term) {
+      updatedQuery.term = term
+    } else {
+      delete updatedQuery.term
+    }
+    router.push({
+      query: updatedQuery,
+    })
   }
 
   return {
